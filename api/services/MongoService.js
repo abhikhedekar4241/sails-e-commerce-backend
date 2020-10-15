fdd330b6f0b35b56e3d594177ad91423222e74b3
Abhishek Khedekar <abhikhedekar4241@gmail.com>
2020-10-15 21:47:12 +0530

    mongo config

--- api/services/MongoService.js
+++ api/services/MongoService.js
@@ -1,18 +1,18 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-let clientRef=null;
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+let clientRef = null;
 // Connection URL
 const url = sails.config.datastores.default.url;
 console.log(url);
 
-MongoClient.connect(url, async(err, client)=> {
+MongoClient.connect(url, { useUnifiedTopology: true }, async (err, client) => {
   assert.equal(null, err);
-  console.log('Connected successfully to server');
-  clientRef=client;
+  console.log("Connected successfully to server");
+  clientRef = client;
   //client.close();
 });
-module.exports={
-  connectData:async function(){
+module.exports = {
+  connectData: async function () {
     return clientRef;
-  }
+  },
 };
